502161f296a28db19bc3463010e416a1e54007b4
Hyorm <21600193@handong.edu>
2017-11-08 20:41:50 +0900

    timeStamp and random

--- src/tsp/main/Main.java
+++ src/tsp/main/Main.java
@@ -6,17 +6,61 @@ import tsp.gui.*;
 import java.awt.*;
 import java.util.*;
 
-public class Main{
+class MultiThread extends Thread{
 
+	long now;
+	Integer[][] node = new Integer[48][48];
+
+	public MultiThread(long now, Integer[][] arr){
+
+		this.now = now;
+		this.node = arr;
+
+	}
+
+	public void run(){
+
+		Path minPath = new Path();
+		Path newPath = new Path();
+
+		HashSet<Integer[]> se = new HashSet<Integer[]>();		
+
+		int generath = 0;
+
+		while(true){
+
+			long endTime = System.currentTimeMillis();
+
+			if((endTime - now)/1000==10) break;
+
+			se = MakeSequence.makeSequence(1);
+			
+			for(Integer[] num: se)
+				newPath = new Path(node, num);
+
+			if(generath==0)
+				minPath = newPath;
+			if(newPath.getWeight()<minPath.getWeight())
+				minPath = newPath;
+
+			generath++;
+		}
+		System.out.println(generath+"Randome Generation Min Weight: "+ minPath.getWeight());
+	}
+}
+
+public class Main{
 	public static void main(String[] args){
 		
 		String str = "";
+
+		long startTime = System.currentTimeMillis();
 		
 		Integer[][] arr = new Integer[48][48];
 	
 		HashSet<Integer[]> sequence = new HashSet<Integer[]>();
 
-		Generation[] allGeneration = new Generation[100];
+		Generation[] allGeneration = new Generation[2];
 
 		Path[] path = new Path[100];
 
@@ -37,17 +81,35 @@ public class Main{
 			}
 						
 			allGeneration[0] = new Generation(path);
-			
-			for(int j =1; j<100; j++){
-			
-				 allGeneration[j] = new Evolution(allGeneration[j-1], arr).getEvolution();
+
+			int j = 1, gen = 0, generationNum = 0;
+
+			MultiThread rand_1 = new MultiThread(startTime, arr);
+
+			rand_1.start();
+			while(true){
+					
+				long endTime = System.currentTimeMillis();
+
+				if((endTime - startTime)/1000==10) break;
+				
+				if(j == 1){
+					allGeneration[j] = new Evolution(allGeneration[j-1], arr).getEvolution();
+				
+					gen = j;
+					j--;
+				}
+				else {
+					allGeneration[j] = new Evolution(allGeneration[j+1], arr).getEvolution();
+					gen = j;
+					j++;
+				}
+				
+				generationNum++;
+
 			}
-
-			for(int j =0; j<100; j++){
-
-				System.out.println(j+" Generation Min Weight: "+ allGeneration[j].getMinWeight());
-
-			}
+	
+			System.out.println(generationNum+" Generation Min Weight: "+ allGeneration[gen].getMinWeight());
 	
 //			ShowData dataHist= new ShowData(allGeneration);
 			
